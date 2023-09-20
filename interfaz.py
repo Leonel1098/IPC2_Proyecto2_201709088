c8a8f75e384262108ff11c69a11d0a59c5bb24b0
Leonel1098 <gonzalezleonel1098@gmail.com>
2023-09-20 00:14:01 -0600

    Interfaz Inicio

---
+++ interfaz.py
@@ -0,0 +1,15 @@
+from tkinter import Tk
+from tkinter import * 
+from tkinter import filedialog
+
+class interfaz ():
+
+    def __init__():
+        
+        ventana = Tk()
+        ventana.title("PROYECTO")
+        ventana.geometry("%dx%d+%d+%d" % (900,500,350,100))
+        ventana.resizable(0,0)
+        ventana.mainloop()
+
+    __init__()
